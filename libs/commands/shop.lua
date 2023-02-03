4d2167f100adff47f2617d4f3e7c6d7c95fd7c90
Lyrthras <defelixanne@gmail.com>
2023-02-04 04:26:36 +0800

    Fixed shop command

--- libs/commands/shop.lua
+++ libs/commands/shop.lua
@@ -91,6 +91,10 @@ function cmd.handle(intr)
         local files = {}
         for i = 1, numFiles do
             files[#files+1] = {('CtBot_Shop%02d_%s.png'):format(i, os.date('!%Y-%m-%d')), fs.readFileSync('storage/Shop'..i..'.png')}
+            if #files >= 8 and #files < numFiles then
+                intr.channel:send {files = files}
+                files = {}
+            end
         end
         intr.channel:send {files = files}
         return
@@ -183,86 +187,84 @@ function cmd.handle(intr)
     local i = 0
     while i < #sectionsSorted do
         i = i + 1
-        do
-            local sectionNameId, sets = sectionsSorted[i][1], sectionsSorted[i][2]
+        local sectionNameId, sets = sectionsSorted[i][1], sectionsSorted[i][2]
 
-            local items = {}
-            local excess = {}
-            for _,set in ipairs(sets) do
-                for _, item in ipairs(set) do
-                    if #items < 6 then
-                        items[#items+1] = item
-                    else
-                        excess[#excess+1] = item
-                    end
-                end
-            end
-            if #excess > 0 then
-                if #items > #excess + 1 then
-                    for _ = 1, math.floor((#items - #excess)/2) do
-                        table.insert(excess, 1, table.remove(items))
-                    end
-                end
-                table.insert(sectionsSorted, i+1, {sectionNameId, {excess}})
-            end
-
-            local shopTitle
-            if sectionNameId:find('\1') then
-                local x = sectionNameId:find('\1')
-                local prefix, name = sectionNameId:sub(1,x-1), sectionNameId:sub(x+1)
-                shopTitle = prefix .. (ct:getLocalization('MENU', name) or 'Other')
-            else
-                shopTitle = ct:getLocalization('MENU', sectionNameId) or 'Other'
-            end
-            local imVars = {
-                ShopTitle = ShopRenderer.imageText('ShopTitle', shopTitle),
-                Footer = footer,
-                CarouselConfig = { centered = true, maxcols = 6, xpad = 80 },
-                Items = {}
-            }
-
-
-            for _, item in ipairs(items) do
-                local pkg = ct:getPackageInfo(item.id)
-                if not pkg then goto continue end
-                local name = ct:getLocalization('PACKAGE', item.id..'_NAME') or '???'
-                local desc = ct:getLocalization('PACKAGE', item.id..'_DESC') or ' '
-                local longDesc = {}
-                longDesc[#longDesc+1] = ct:getLocalization('PACKAGE', item.id..'_SPECDESC')
-                longDesc[#longDesc+1] = ct:getLocalization('PACKAGE', item.id..'_CONTENT')
-
-                local contType = item.square and 'Square' or 'Vert'
-                local thumbName = pkg.texPaths.ShopMenuIcon and pkg.texPaths.ShopMenuIcon:match('[^.]+$') or (contType .. 'Placeholder')
-                local vars = {
-                    ThumbImage = ShopRenderer.imageBinary(ct:getThumbnail(thumbName .. '.png')),
-                    ItemTitle = ShopRenderer.imageText('ItemTitle', util.locresFmt(name:upper())),
-                    ItemDescShort = ShopRenderer.imageText('ItemDescShort', util.locresFmt(desc)),
-                    ItemDescLong = ShopRenderer.imageLongDesc(util.locresFmt(table.concat(longDesc, '\n\n'))),
-                    ContainerType = contType,
-                    Rarity = pkg.rarity,
-                }
-                imVars.Items[#imVars.Items+1] = vars
-
-                if item.RM > 0 or (item.SC == 0 and item.HC == 0) then
-                    vars.PriceType = 'None'
-                    vars.RealPrice = ShopRenderer.imageText('Price', item.RM == 0 and 'FREE' or ('€'..(item.RM/100)))
-                elseif item.SC == 0 then
-                    vars.PriceType = 'Gems'
-                    vars.GemsPrice = ShopRenderer.imageText('Price', item.HC)
-                elseif item.HC == 0 then
-                    vars.PriceType = 'Coins'
-                    vars.CoinsPrice = ShopRenderer.imageText('Price', item.SC)
+        local items = {}
+        local excess = {}
+        for _,set in ipairs(sets) do
+            for _, item in ipairs(set) do
+                if #items < 6 then
+                    items[#items+1] = item
                 else
-                    vars.PriceType = 'Both'
-                    vars.CoinsPrice = ShopRenderer.imageText('Price', item.SC)
-                    vars.GemsPrice = ShopRenderer.imageText('Price', item.HC)
+                    excess[#excess+1] = item
                 end
+            end
+        end
+        if #excess > 0 then
+            if #items > #excess + 1 then
+                for _ = 1, math.floor((#items - #excess)/2) do
+                    table.insert(excess, 1, table.remove(items))
+                end
+            end
+            table.insert(sectionsSorted, i+1, {sectionNameId, {excess}})
+        end
 
-                ::continue::
+        local shopTitle
+        if sectionNameId:find('\1') then
+            local x = sectionNameId:find('\1')
+            local prefix, name = sectionNameId:sub(1,x-1), sectionNameId:sub(x+1)
+            shopTitle = prefix .. (ct:getLocalization('MENU', name) or 'Other')
+        else
+            shopTitle = ct:getLocalization('MENU', sectionNameId) or 'Other'
+        end
+        local imVars = {
+            ShopTitle = ShopRenderer.imageText('ShopTitle', shopTitle),
+            Footer = footer,
+            CarouselConfig = { centered = true, maxcols = 6, xpad = 80 },
+            Items = {}
+        }
+
+
+        for _, item in ipairs(items) do
+            local pkg = ct:getPackageInfo(item.id)
+            if not pkg then goto continue end
+            local name = ct:getLocalization('PACKAGE', item.id..'_NAME') or '???'
+            local desc = ct:getLocalization('PACKAGE', item.id..'_DESC') or ' '
+            local longDesc = {}
+            longDesc[#longDesc+1] = ct:getLocalization('PACKAGE', item.id..'_SPECDESC')
+            longDesc[#longDesc+1] = ct:getLocalization('PACKAGE', item.id..'_CONTENT')
+
+            local contType = item.square and 'Square' or 'Vert'
+            local thumbName = pkg.texPaths.ShopMenuIcon and pkg.texPaths.ShopMenuIcon:match('[^.]+$') or (contType .. 'Placeholder')
+            local vars = {
+                ThumbImage = ShopRenderer.imageBinary(ct:getThumbnail(thumbName .. '.png')),
+                ItemTitle = ShopRenderer.imageText('ItemTitle', util.locresFmt(name:upper())),
+                ItemDescShort = ShopRenderer.imageText('ItemDescShort', util.locresFmt(desc)),
+                ItemDescLong = ShopRenderer.imageLongDesc(util.locresFmt(table.concat(longDesc, '\n\n'))),
+                ContainerType = contType,
+                Rarity = pkg.rarity,
+            }
+            imVars.Items[#imVars.Items+1] = vars
+
+            if item.RM > 0 or (item.SC == 0 and item.HC == 0) then
+                vars.PriceType = 'None'
+                vars.RealPrice = ShopRenderer.imageText('Price', item.RM == 0 and 'FREE' or ('€'..(item.RM/100)))
+            elseif item.SC == 0 then
+                vars.PriceType = 'Gems'
+                vars.GemsPrice = ShopRenderer.imageText('Price', item.HC)
+            elseif item.HC == 0 then
+                vars.PriceType = 'Coins'
+                vars.CoinsPrice = ShopRenderer.imageText('Price', item.SC)
+            else
+                vars.PriceType = 'Both'
+                vars.CoinsPrice = ShopRenderer.imageText('Price', item.SC)
+                vars.GemsPrice = ShopRenderer.imageText('Price', item.HC)
             end
 
-            ShopRenderer.generate(imVars, 'storage/Shop'..i..'.png')
+            ::continue::
         end
+
+        ShopRenderer.generate(imVars, 'storage/Shop'..i..'.png')
     end
 
     lastHash = hash
@@ -273,7 +275,7 @@ function cmd.handle(intr)
 
     local files = {}
     numFiles = #sectionsSorted
-    for _ = 1, numFiles do
+    for i = 1, numFiles do
         files[#files+1] = {('CtBot_Shop%02d_%s.png'):format(i, os.date('!%Y-%m-%d')), fs.readFileSync('storage/Shop'..i..'.png')}
         if #files >= 8 and #files < numFiles then
             intr.channel:send {files = files}
